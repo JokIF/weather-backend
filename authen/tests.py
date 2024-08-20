9e3e87af568a25b5c2de427664d417e2c9ef9eb2
Ilya Fedorov <jokifoki2@gmail.com>
2024-08-20 15:17:03 +0700

    Add TGUser create test

--- authen/tests.py
+++ authen/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.response import Response
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+from authen.models import TGOwner, TGUser
+from authen.views import OwnerViewSet
+
+
+class ViewTest(TestCase):
+    @staticmethod
+    def get_test_data_create_view():
+        return (
+            "/api/owners/",
+            {"user_id": 111, "username": "JohnCerlik"},
+            {"username": "JohnCerlik", "user_id": 111, "first_name": "", "last_name": ""}
+        )
+    def test_create_view(self):
+        test_path, test_data, test_correct_data = self.get_test_data_create_view()
+        
+        client = Client()
+        response: Response = client.post(path=test_path, data=test_data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, test_correct_data)
+        
